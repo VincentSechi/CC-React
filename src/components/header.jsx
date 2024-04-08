7ec39144f81fda48cf0d98b61f53c9caf2f121d9
Vincent <sechivincent@gmail.com>
2024-04-08 14:30:49 +0200

    désolé j'ai mis du style

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,9 +3,13 @@ import styles from '../styles/_header.module.css'
 const Header = () => {
   return (
     <header className={styles.header}>
-        <img className={styles.headerImg} src='https://cdn.pixabay.com/photo/2018/03/26/14/18/man-
+      <img className={styles.headerImg} src='https://cdn.pixabay.com/photo/2018/03/26/14/18/man-
 3262834_960_720.png' alt='img' />
-        <a href="/">Accueil</a>
+      <nav>
+        <li>
+          <a href="/">Accueil</a>
+        </li>
+      </nav>
     </header>
   )
 }
