670c98b0023979e7f008319ca8f15654a61ae4d4
Vincent <sechivincent@gmail.com>
2024-04-09 15:10:44 +0200

    page 404

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Layout from "./components/Layout";
 
 import Home from "./pages/Home";
 import DishDetails from "./pages/DishDetails";
+import NotFound from "./pages/NotFound";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
         <Routes>
-          <Route element={<Layout />}>
+          <Route element={<Layout />}>s
             <Route index element={<Home />} />
             <Route path="/plat/:slug" element={<DishDetails />} />
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
     </div>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const NotFound = () => {
+  return (
+    <Container>
+        <Row>
+            <Col>
+                <h1>Page non trouvée</h1>
+            </Col>
+        </Row>
+    </Container>
+  )
+}
+
+export default NotFound
